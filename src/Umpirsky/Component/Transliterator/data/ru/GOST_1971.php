97d2e7306f23b767ffc8e17a5a27d4ed6a460c0f
umpirsky <umpirsky@gmail.com>
2011-01-25 16:07:20 +0100

    Added README.

---
+++ src/Umpirsky/Component/Transliterator/data/ru/GOST_1971.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * This file is part of the Umpirsky components.
+ *
+ * (c) Saša Stamenković <umpirsky@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * Russian mapping (GOST 1971 system).
+ *
+ * @see http://en.wikipedia.org/wiki/Romanization_of_Russian
+ */
+return array (
+    'cyr' => array('я', 'ю', 'щ', 'ц', 'ч', 'ш', 'ё', 'ж', 'Э', 'Ё', 'Ж', 'Ц', 'Ч', 'Ш', 'Я', 'Ю', 'Щ', 'Ы', 'э', 'ь', 'ы', 'ъ', 'а', 'б', 'в', 'г', 'д', 'е', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'Ь', 'Ъ', 'А', 'Б', 'В', 'Г', 'Д', 'Е', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х'),
+    'lat' => array('ya', 'yu', 'shh', 'cz', 'ch', 'sh', 'yo', 'zh', 'Eh', 'Yo', 'Zh', 'Cz', 'Ch', 'Sh', 'Ya', 'Yu', 'Shh', "Y'", 'eh', 'ʹ', "y'", 'ʺ', 'a', 'b', 'v', 'g', 'd', 'e', 'z', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'f', 'x', 'ʹ', 'ʺ', 'A', 'B', 'V', 'G', 'D', 'E', 'Z', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'F', 'X')
+);
---
+++ src/Umpirsky/Component/Transliterator/data/ru/ISO_R_9_1968.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * This file is part of the Umpirsky components.
+ *
+ * (c) Saša Stamenković <umpirsky@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * Russian mapping (ISO/R 9:1968 system).
+ *
+ * @see http://en.wikipedia.org/wiki/Romanization_of_Russian
+ */
+return array (
+    'cyr' => array('я', 'ю', 'щ', 'х', 'Я', 'Ю', 'Щ', 'Х', 'э', 'ь', 'ы', 'ъ', 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'ц', 'ч', 'ш', 'Э', 'Ь', 'Ы', 'Ъ', 'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Ц', 'Ч', 'Ш'),
+    'lat' => array('ja', 'ju', 'šč', 'ch', 'Ja', 'Ju', 'Šč', 'Ch', 'ė', 'ʹ', 'y', 'ʺ', 'a', 'b', 'v', 'g', 'd', 'e', 'ë', 'ž', 'z', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'f', 'c', 'č', 'š', 'Ė', 'ʹ', 'Y', 'ʺ', 'A', 'B', 'V', 'G', 'D', 'E', 'Ë', 'Ž', 'Z', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'F', 'C', 'Č', 'Š')
+);
