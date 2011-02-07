29ce59cf1571997b853e766996dc6c65c1669dff
umpirsky <umpirsky@gmail.com>
2011-02-07 15:27:42 +0100

    Added some ukrainian transliteration systems.

---
+++ src/Umpirsky/Component/Transliterator/data/uk/Derzhstandart_1995.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * This file is part of the Umpirsky components.
+ *
+ * (c) Saša Stamenković <umpirsky@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * Russian mapping (Derzhstandart 1995 system).
+ *
+ * @see http://en.wikipedia.org/wiki/Romanization_of_Ukrainian
+ */
+return array (
+    'cyr' => array(
+        'щ', 'г', 'є', 'ж', 'ї', 'х', 'ч', 'ш', 'ю', 'я',
+        'Щ', 'Г', 'Є', 'Ж', 'Ї', 'Х', 'Ч', 'Ш', 'Ю', 'Я',
+        'а', 'б', 'в', 'ґ', 'д', 'е', 'з', 'и', 'і', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'ц', 'ь',
+        'А', 'Б', 'В', 'Ґ', 'Д', 'Е', 'З', 'И', 'І', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Ц', 'Ь'
+    ),
+    'lat' => array(
+        'shh', 'gh', 'je', 'zh', 'ji', 'kh', 'ch', 'sh', 'ju', 'ja',
+        'Shh', 'Gh', 'Je', 'Zh', 'Ji', 'Kh', 'Ch', 'Sh', 'Ju', 'Ja',
+        'a', 'b', 'v', 'g', 'd', 'e', 'z', 'y', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'f', 'c', 'j',
+        'A', 'B', 'V', 'G', 'D', 'E', 'Z', 'Y', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'F', 'C', 'J'
+    )
+);
---
+++ src/Umpirsky/Component/Transliterator/data/uk/GOST_1986.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * This file is part of the Umpirsky components.
+ *
+ * (c) Saša Stamenković <umpirsky@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * Russian mapping (GOST 1986 system).
+ *
+ * @see http://en.wikipedia.org/wiki/Romanization_of_Ukrainian
+ */
+return array (
+    'cyr' => array(
+        'є', 'щ', 'ю', 'я',
+        'Є', 'Щ', 'Ю', 'Я',
+        'а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'і', 'ї', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'ь',
+        'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ж', 'З', 'И', 'І', 'Ї', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Ь'
+    ),
+    'lat' => array(
+        'je', 'šč', 'ju', 'ja',
+        'Je', 'Šč', 'Ju', 'Ja',
+        'a', 'b', 'v', 'g', 'd', 'e', 'ž', 'z', 'i', 'i', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'f', 'h', 'c', 'č', 'š', "'",
+        'A', 'B', 'V', 'G', 'D', 'E', 'Ž', 'Z', 'I', 'I', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'F', 'H', 'C', 'Č', 'Š', "'"
+    )
+);
