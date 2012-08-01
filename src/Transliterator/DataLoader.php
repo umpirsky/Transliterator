03d4290c2788d77e952edac3add2a11b9ac27bae
Sasa Stamenkovic <umpirsky@gmail.com>
2012-08-01 20:53:45 +0200

    Updated copyright.

--- src/Transliterator/DataLoader.php
+++ src/Transliterator/DataLoader.php
@@ -1,9 +1,9 @@
 <?php
 
 /*
- * This file is part of the Umpirsky components.
+ * This file is part of the Transliterator package.
  *
- * (c) Saša Stamenković <umpirsky@gmail.com>
+ * (c) Саша Стаменковић <umpirsky@gmail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -14,7 +14,7 @@ namespace Transliterator;
 /**
  * Loads transliteration maps from files.
  *
- * @author Saša Stamenković <umpirsky@gmail.com>
+ * @author Саша Стаменковић <umpirsky@gmail.com>
  */
 class DataLoader {
     /**
--- src/Transliterator/Transliterator.php
+++ src/Transliterator/Transliterator.php
@@ -1,9 +1,9 @@
 <?php
 
 /*
- * This file is part of the Umpirsky components.
+ * This file is part of the Transliterator package.
  *
- * (c) Saša Stamenković <umpirsky@gmail.com>
+ * (c) Саша Стаменковић <umpirsky@gmail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -24,7 +24,8 @@ namespace Transliterator;
  *     $transliterator->cyr2Lat('Ниш'); // returns 'Niš'
  *     $transliterator->lat2Cyr('Niš'); // returns 'Ниш'
  *
- * @author Saša Stamenković <umpirsky@gmail.com>
+ * @author Саша Стаменковић <umpirsky@gmail.com>
+ * 
  * @see http://en.wikipedia.org/wiki/Transliteration
  */
 class Transliterator {
